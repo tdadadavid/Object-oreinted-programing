5a4d6672237c99a18583b4baa9127a277cfb1470
tdadadavid <davidtofunmidada@gamil.com>
2020-12-10 14:12:39 +0100

    I created two extra classes videos and VideosCollection. VideosCollection inherits from collection class all its attribute

--- index.php
+++ index.php
@@ -1,7 +1,58 @@
 <?php
 
-class collection{
+class collection
+{
+    public array $items;
+
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+
+    public function sum($key)
+    {
+
+      return  array_sum(array_map(function ($items) use ($key)
+        {
+            return $items->$key;
+        } , $this->items));
+    }
 
 }
 
 
+class VideosCollection extends collection
+{
+    public function length(){
+        return $this->sum("length");
+    }
+}
+
+
+class videos
+{
+    public string $title;
+    public int $length;
+
+    /**
+     * videos constructor.
+     * @param string $title
+     * @param int $length
+     */
+
+    public function __construct(string $title, int $length)
+    {
+        $this->title = $title;
+        $this->length = $length;
+    }
+}
+
+$videos = new VideosCollection([
+    new videos("House on fire" , 60),
+    new videos("Abejoye" , 100),
+    new videos("Abattoir" , 300)
+]);
+
+echo $videos->length();
