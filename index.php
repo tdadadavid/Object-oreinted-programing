480ddf3c94a203850cba872c6b0e8c16bf2003b2
tdadadavid <davidtofunmidada@gamil.com>
2020-12-10 15:05:03 +0100

    I created two extra classes that inherited from the AchievementType class and also put some functionalities in some methods

--- index.php
+++ index.php
@@ -2,4 +2,35 @@
 
 class AchievementType{
 
+    public function name()
+    {
+        $class = (new ReflectionClass($this))->getShortName();
+
+        return trim(preg_replace('/[A-Z]/', ' $0' , $class));
+    }
+
+    public function icon()
+    {
+        return strtolower(str_replace(' ' , '-' , $this->name())) . '.png';
+    }
+
 }
+
+class FirstThousandPoints extends AchievementType
+{
+    public function qaulifier($user){
+        return null;
+    }
+}
+
+class  FirstBestAnswer extends AchievementType
+{
+    public function qaulifier($user){
+        return null;
+    }
+
+}
+
+
+$achievement = new FirstThousandPoints();
+echo $achievement->icon();
