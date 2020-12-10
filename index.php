1cacab5c7b92f3039607736dc0cc5e8679069560
tdadadavid <davidtofunmidada@gamil.com>
2020-12-10 11:43:45 +0100

    I gave my team a name and also added new members to my team

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
 
 class Team  {
-    protected $name;
+    protected string $name;
+    protected array $members = [];
 
     public function __construct($name)
     {
@@ -11,22 +12,31 @@ class Team  {
     }
 
     public function getName(){
-
+      return  $this->name;
     }
 
     public function members(){
-
+        return $this->members;
     }
 
-    public  function isAMember(){
-
+    public  function isAMember($name){
+        if (! array_key_exists($name , $this->members)){
+            return "Not a member of this team";
+        }
     }
 
-    public function addMember(){
-
+    public function addMember($name){
+        $this->members[] = $name;
     }
-
 }
+
+$barcelona = new Team("Barca");
+
+$barcelona->addMember("Lionel Messi");
+$barcelona->addMember("Cristina Ronaldo");
+var_dump($barcelona);
+
+
 
 
 
