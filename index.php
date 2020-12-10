b799498712c20fc68428d7b6e4c497ce85fc9e92
tdadadavid <davidtofunmidada@gamil.com>
2020-12-10 14:36:26 +0100

    I closed my collection class

--- index.php
+++ index.php
@@ -1,55 +1,6 @@
 <?php
 
-class collection
-{
-    public array $items;
+class AchivementType{
 
-
-    public function __construct(array $items)
-    {
-        $this->items = $items;
-    }
-
-
-    public function sum($key)
-    {
-
-      return  array_sum(array_column($this->items , $key));
-    }
 
 }
-
-
-class VideosCollection extends collection
-{
-    public function length(){
-        return $this->sum("length");
-    }
-}
-
-
-class videos
-{
-    public string $title;
-    public int $length;
-
-    /**
-     * videos constructor.
-     * @param string $title
-     * @param int $length
-     */
-
-    public function __construct(string $title, int $length)
-    {
-        $this->title = $title;
-        $this->length = $length;
-    }
-}
-
-$videos = new VideosCollection([
-    new videos("House on fire" , 60),
-    new videos("Abejoye" , 100),
-    new videos("Abattoir" , 300)
-]);
-
-echo $videos->length();
